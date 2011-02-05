cf48dc3ba9758d8aeb548da0d5c7bb0360698daa
Doug Hellmann <doug.hellmann@gmail.com>
2011-02-05 13:41:26 -0500

    clean up for presentation

--- hidden_stdlib/sqlite3_custom_type.py
+++ hidden_stdlib/sqlite3_custom_type.py
@@ -1,32 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
 """Defining a custom type.
 """
-#end_pymotw_header
 
 import sqlite3
-try:
-    import cPickle as pickle
-except:
-    import pickle
-
-db_filename = 'todo.db'
-
-def adapter_func(obj):
-    """Convert from in-memory to storage representation.
-    """
-    print 'adapter_func(%s)\n' % obj
-    return pickle.dumps(obj)
-
-def converter_func(data):
-    """Convert from storage to in-memory representation.
-    """
-    print 'converter_func(%r)\n' % data
-    return pickle.loads(data)
-
+import pickle
 
 class MyObj(object):
     def __init__(self, arg):
@@ -35,17 +13,23 @@ class MyObj(object):
         return 'MyObj(%r)' % self.arg
 
 # Register the functions for manipulating the type.
+
+def adapter_func(obj):
+    """memory -> storage"""
+    print 'Saving:', obj
+    return pickle.dumps(obj)
+
 sqlite3.register_adapter(MyObj, adapter_func)
+
+def converter_func(data):
+    """storage -> memory"""
+    return pickle.loads(data)
+    
 sqlite3.register_converter("MyObj", converter_func)
 
-# Create some objects to save.  Use a list of tuples so
-# the sequence can be passed directly to executemany().
-to_save = [ (MyObj('this is a value to save'),),
-            (MyObj(42),),
-            ]
-
-with sqlite3.connect(db_filename,
-                     detect_types=sqlite3.PARSE_DECLTYPES) as conn:
+with sqlite3.connect(
+        'type_demo.db',
+        detect_types=sqlite3.PARSE_DECLTYPES) as conn:
     # Create a table with column of type "MyObj"
     conn.execute("""
     create table if not exists obj (
@@ -56,10 +40,16 @@ with sqlite3.connect(db_filename,
     cursor = conn.cursor()
 
     # Insert the objects into the database
-    cursor.executemany("insert into obj (data) values (?)", to_save)
+    print 'Inserting:'
+    to_save = [ (MyObj('this is a value to save'),),
+                (MyObj(42),),
+                ]
+    cursor.executemany(
+        "insert into obj (data) values (?)", 
+        to_save)
 
     # Query the database for the objects just saved
+    print '\nQuerying:'
     cursor.execute("select id, data from obj")
     for obj_id, obj in cursor.fetchall():
-        print 'Retrieved', obj_id, obj, type(obj)
-        print
+        print 'Retrieved', obj_id, type(obj), obj
